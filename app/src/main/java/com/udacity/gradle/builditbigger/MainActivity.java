9bfd411736d2009d30cb94a6ea21b3357bc3c561
joaobiriba <glaquidara@gmail.com>
2015-10-22 21:59:44 +0200

    Step 1: Create Java library to tell joke

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,13 +7,19 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.laquysoft.joketelling.JokeTeller;
+
 
 public class MainActivity extends ActionBarActivity {
+
+    JokeTeller jokeTeller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        jokeTeller = new JokeTeller();
     }
 
 
@@ -40,7 +46,7 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view){
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, jokeTeller.tellJoke(), Toast.LENGTH_SHORT).show();
     }
 
 
---
+++ joketelling/src/main/java/com/laquysoft/joketelling/JokeTeller.java
@@ -0,0 +1,13 @@
+package com.laquysoft.joketelling;
+
+
+/**
+ * Created by joaobiriba on 22/10/2015.
+ */
+public class JokeTeller {
+
+    public String tellJoke(){
+        return "derp";
+    }
+
+}
